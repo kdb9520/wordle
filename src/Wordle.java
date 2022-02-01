d0d8bb4c2710b04f93bdff605c91220c322d2d68
Kellen Bell <kdb9520@rit.edu>
2022-02-01 14:20:32 -0500

    Fixed some read in problems.

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,20 +1,35 @@
 package src;
 
+import java.io.File;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Wordle {
     public static void main(String[] args) {
-        new Wordle();
+        String word = null;
+        try {
+            Scanner input = new Scanner("C:\\Users\\kelle\\IdeaProjects\\wordle\\src\\dict.txt");
+            File file = new File(input.nextLine());
+            input = new Scanner(file);
+            int stopPoint = (int) (Math.random() * 5757);
+            int i = 0;
+            while (input.hasNextLine() && i < stopPoint) {
+                word = input.nextLine();
+                i++;
+            }
+            input.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        new Wordle(word);
     }
 
-    public Wordle() {
-        runGame();
+    public Wordle(String word) {
+        runGame(word);
     }
 
-    public void runGame() {
-        SecretWord word = new SecretWord(null);
-        ArrayList<String> secret = word.generateSecret();
-        word.setSecret(secret);
-        System.out.println(word.getSecret());
+    public void runGame(String word) {
+        SecretWord secretWord = new SecretWord(null);
+        ArrayList<String> secret = secretWord.generateSecret();
     }
 }
