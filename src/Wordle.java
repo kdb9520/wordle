deb876ac8c0bcf7bfeb07eaff0b978ef15797577
Kellen Bell <kdb9520@rit.edu>
2022-01-31 21:23:12 -0500

    Implemented some of Wordle.java.

--- src/Wordle.java
+++ src/Wordle.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class Wordle {
     public static void main(String[] args) {
+        new Wordle();
+    }
+
+    public Wordle() {
+        runGame();
+    }
+
+    public void runGame() {
         SecretWord word = new SecretWord(null);
         ArrayList<String> secret = word.generateSecret();
         word.setSecret(secret);
